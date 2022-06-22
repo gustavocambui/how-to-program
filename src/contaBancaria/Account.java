f12774ab988eff2e7861575c7c17a29cf2f6fed1
Gustavo <jgustavocambui@gmail.com>
2022-06-21 21:46:24 -0300

    incrementos.

--- src/contaBancaria/Account.java
+++ src/contaBancaria/Account.java
@@ -1,17 +1,44 @@
 package contaBancaria;
 
+import java.util.Scanner;
+
 public class Account 
 {
 	private String name;
+	private double balance;
 	
-		public void setName(String name)
-		{
+	public Account (String name, double balance) {
+		
 		this.name = name;
-		}
+		if (balance > 0.0)
+			this.balance = balance;
+	}
 	
-		public String getName()
-		{
+	public void withdraw(double retiraDinheiro) {
+		
+		if(retiraDinheiro > balance) 
+			System.out.println("Valor de débito excedeu o saldo de sua conta.");
+		balance = balance - retiraDinheiro;
+	}
+	
+	public void deposit(double depositAmount) {
+		
+		if(depositAmount > 0.0)
+			balance = balance + depositAmount;
+	}
+	
+	public double getBalance() {
+		
+		return balance;
+	}
+	public void setName(String name) {
+		
+		this.name = name;
+	}
+	
+	public String getName() {
+		
 		return name;
-		}
+	}
 	
 }
--- src/contaBancaria/AccountTest.java
+++ src/contaBancaria/AccountTest.java
@@ -8,19 +8,42 @@ public class AccountTest
 	public static void main(String[] args) 
 	{
 		
-		Scanner input = new Scanner (System.in);
+		Account account1 = new Account("Gumball Watterson", 55.00);
+		Account account2 = new Account("Darwin Watterson", -13.00);
 		
-		Account myAccount = new Account();
+		displayAccount(account1);
+		displayAccount(account2);
 		
-		System.out.printf("Nome inicial: %s%n%n", myAccount.getName());
+		depositAccount(account1);
+		depositAccount(account2);
 		
-		System.out.println("Por favor, digite um nome: ");
-		String newName = input.nextLine(); 
-		myAccount.setName(newName); 
-		System.out.println();
-	
-		System.out.printf("Nome armazenado: %s%n%n", myAccount.getName());
+		withdrawAccount(account1);
+		withdrawAccount(account2);
+		
+		displayAccount(account1);
+		displayAccount(account2);
 
 	}
-
+	public static void displayAccount(Account accountToDisplay) {
+		
+		System.out.printf("%s%nSaldo na conta: R$%.2f %n",accountToDisplay.getName(), accountToDisplay.getBalance());
+	}
+	
+	public static void depositAccount(Account accountToDeposit) {
+		
+		Scanner input = new Scanner (System.in);
+		System.out.println(accountToDeposit.getName() + ", entre com valor para depósito em sua conta: ");
+		double depositAmount = input.nextDouble();
+		System.out.printf("%nAdicionando %.2f ao saldo de sua conta...%n%n", depositAmount);
+		accountToDeposit.deposit(depositAmount);
+	}
+	
+	public static void withdrawAccount(Account accountToWithdraw) {
+		
+		Scanner input = new Scanner (System.in);
+		System.out.println(accountToWithdraw.getName() + ", digite o valor que deseja sacar: ");
+		double retirarDinheiro = input.nextDouble();
+		System.out.printf("%nSacando %.2f do saldo de sua conta...%n%n", retirarDinheiro);
+		accountToWithdraw.withdraw(retirarDinheiro);
+	}
 }
